4ecece4c76b01a58e9ca80d699674da816038387
DiegoPereiro <diegocfpereiro@gmail.com>
2019-01-27 17:48:18 +0100

    Enviar datos desde la activity principal a la segunda Activity

--- app/src/main/java/freijo/castro/diego/tareapmdm03/MainActivity.java
+++ app/src/main/java/freijo/castro/diego/tareapmdm03/MainActivity.java
@@ -2,6 +2,7 @@ package freijo.castro.diego.tareapmdm03;
 
 import android.content.Intent;
 import android.content.res.Configuration;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,17 +12,21 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private static final String LOG_TAG=MainActivity.class.getSimpleName();
+    public static final int TEXTO_RESPUESTA=1;
 
     private EditText etMensaje;
+    private TextView tvRespuesta, tvTitulo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.e("MainActivity", "Hola mundo");
-
         etMensaje=(EditText) findViewById(R.id.etMensaje);
+        tvTitulo=(TextView) findViewById(R.id.tvTitulo);
+        tvRespuesta=(TextView) findViewById(R.id.tvRespuesta);
+
+        Log.e("MainActivity", "Hola mundo");
 
     }
 
@@ -31,6 +36,22 @@ public class MainActivity extends AppCompatActivity {
         Intent intent=new Intent(this, SegundaActivity.class);
         String mensaje=etMensaje.getText().toString();
         intent.putExtra("Mensaje", mensaje);
-        startActivity(intent);
+        startActivityForResult(intent, TEXTO_RESPUESTA);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == TEXTO_RESPUESTA) {
+
+            if (resultCode == RESULT_OK) {
+
+                String respuesta =data.getStringExtra(SegundaActivity.EXTRA_RESPUESTA);
+                tvTitulo.setVisibility(View.VISIBLE);
+                tvRespuesta.setText(respuesta);
+                tvRespuesta.setVisibility(View.VISIBLE);
+            }
+        }
     }
 }
--- app/src/main/java/freijo/castro/diego/tareapmdm03/SegundaActivity.java
+++ app/src/main/java/freijo/castro/diego/tareapmdm03/SegundaActivity.java
@@ -3,11 +3,17 @@ package freijo.castro.diego.tareapmdm03;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
-public class SegundaActivity extends AppCompatActivity {
+import java.security.PublicKey;
 
-    private TextView tvRecibir;
+public class SegundaActivity extends AppCompatActivity {
+    public static final String EXTRA_RESPUESTA ="com.example.android.twoactivities.extra.REPLY";
+
+    private TextView tvMensaje;
+    private EditText etRespuesta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,8 +21,21 @@ public class SegundaActivity extends AppCompatActivity {
         setContentView(R.layout.activity_segunda);
 
         Intent intent=getIntent();
-        tvRecibir=(TextView) findViewById(R.id.txtRecibir);
+        tvMensaje=(TextView) findViewById(R.id.tvMensaje);
+        etRespuesta=(EditText) findViewById(R.id.etRespuesta);
+
+
+
         String mensaje=intent.getStringExtra("Mensaje");
-        tvRecibir.setText(mensaje);
+        tvMensaje.setText(mensaje);
+    }
+    public void respuesta(View view){
+        String respuesta=etRespuesta.getText().toString();
+
+        Intent intent=new Intent();
+        intent.putExtra(EXTRA_RESPUESTA, respuesta);
+        setResult(RESULT_OK, intent);
+        finish();
+
     }
 }
