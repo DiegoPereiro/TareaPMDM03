6c2aa34aa9f262203b6cc91cab1b8fba4e1a365b
DiegoPereiro <diegocfpereiro@gmail.com>
2019-01-27 15:24:01 +0100

    Crear segunda activity

--- app/src/main/java/freijo/castro/diego/tareapmdm03/MainActivity.java
+++ app/src/main/java/freijo/castro/diego/tareapmdm03/MainActivity.java
@@ -1,10 +1,13 @@
 package freijo.castro.diego.tareapmdm03;
 
+import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG=MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +15,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Log.e("MainActivity", "Hola mundo");
+    }
+
+    public void lanzarSegundaActivity(View view){
+        Log.d(LOG_TAG, "Boton segunda activity");
+
     }
 }
