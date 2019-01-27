2554feae3bf9b2ab0acd66429f81472cb3810758
DiegoPereiro <diegocfpereiro@gmail.com>
2019-01-27 15:49:34 +0100

    Enviar datos desde la activity principal a la segunda Activity

--- app/src/main/java/freijo/castro/diego/tareapmdm03/MainActivity.java
+++ app/src/main/java/freijo/castro/diego/tareapmdm03/MainActivity.java
@@ -6,9 +6,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private static final String LOG_TAG=MainActivity.class.getSimpleName();
+
+    private EditText etMensaje;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +20,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Log.e("MainActivity", "Hola mundo");
+
+        etMensaje=(EditText) findViewById(R.id.etMensaje);
+
     }
 
     public void lanzarSegundaActivity(View view){
         Log.d(LOG_TAG, "Boton segunda activity");
+
         Intent intent=new Intent(this, SegundaActivity.class);
+        String mensaje=etMensaje.getText().toString();
+        intent.putExtra("Mensaje", mensaje);
         startActivity(intent);
     }
 }
--- app/src/main/java/freijo/castro/diego/tareapmdm03/SegundaActivity.java
+++ app/src/main/java/freijo/castro/diego/tareapmdm03/SegundaActivity.java
@@ -1,13 +1,22 @@
 package freijo.castro.diego.tareapmdm03;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SegundaActivity extends AppCompatActivity {
+
+    private TextView tvRecibir;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_segunda);
+
+        Intent intent=getIntent();
+        tvRecibir=(TextView) findViewById(R.id.txtRecibir);
+        String mensaje=intent.getStringExtra("Mensaje");
+        tvRecibir.setText(mensaje);
     }
 }
